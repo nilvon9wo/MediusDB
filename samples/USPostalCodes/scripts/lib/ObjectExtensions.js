acca04878f1ab830eef1ea561d2ad5af2f79645a
Brian <kessler.bm@gmail.com>
2016-09-03 16:50:36 +0200

    fixed enumeration of Object.toArray

--- samples/USPostalCodes/scripts/lib/ObjectExtensions.js
+++ samples/USPostalCodes/scripts/lib/ObjectExtensions.js
@@ -1,10 +1,20 @@
+Object.prototype.method = function (name, func, override) {
+    'use strict';
+    if (override || !this.prototype[name]) {
+        this.prototype[name] = func;
+        Object.defineProperty(this.prototype, name, {enumerable: false});
+        return this;
+    }
+};
+Object.defineProperty(Object.prototype, 'method', {enumerable: false});
+
 Object.toArray = function (array, startIndex) {
     'use strict';
     startIndex = startIndex || 0;
     return Array.prototype.slice.call(array, startIndex);
 };
 
-Object.prototype.toArray = function (startIndex) {
+Object.method('toArray', function (startIndex){
     'use strict';
     return Object.toArray(this, startIndex);
-};
+});
